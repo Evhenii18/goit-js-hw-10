6adbcbc6d7c32a007b8d9f43a37a1ea007f7989d
Yevhenii Baranik <baranik18@gmail.com>
2024-09-07 21:00:48 +0100

    init

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,20 +1,27 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-// Вибираємо елементи форми
+// Находим элементы формы
 const form = document.querySelector('.form');
-const delayInput = form.elements['delay'];
-const stateInputs = form.elements['state'];
+const delayInput = document.querySelector('input[name="delay"]');
+const stateInputs = document.querySelectorAll('input[name="state"]');
 
-// Обробка сабміту форми
-form.addEventListener('submit', function (event) {
-	event.preventDefault();
+// Обработчик события для отправки формы
+form.addEventListener('submit', (event) => {
+	event.preventDefault(); // Отключаем перезагрузку страницы
 
-	// Отримуємо значення з форми
-	const delay = Number(delayInput.value);
-	const selectedState = stateInputs.value;
+	// Получаем значение задержки из input
+	const delay = parseInt(delayInput.value, 10);
 
-	// Створюємо проміс з затримкою
+	// Получаем выбранное состояние промиса (fulfilled/rejected)
+	let selectedState;
+	stateInputs.forEach(input => {
+		if (input.checked) {
+			selectedState = input.value;
+		}
+	});
+
+	// Создаем промис с заданной задержкой
 	createPromise(delay, selectedState)
 		.then((delay) => {
 			iziToast.success({
@@ -30,7 +37,7 @@ form.addEventListener('submit', function (event) {
 		});
 });
 
-// Функція створення промісу
+// Функция для создания промиса
 function createPromise(delay, state) {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
